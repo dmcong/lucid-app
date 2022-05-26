c1ab6406a4be764517589782cf7c24dbb01fa3fc
dmcong <83624415+dmcong@users.noreply.github.com>
2022-05-26 18:32:48 +0700

    Update index.tsx

--- src/app/view/index.tsx
+++ src/app/view/index.tsx
@@ -1,23 +1,9 @@
-import { useCallback, useEffect, useMemo } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useWallet } from '@senhub/providers'
-
-import { Row, Col, Typography, Button, Space } from 'antd'
-import IonIcon from '@sentre/antd-ionicon'
-
-import BG from 'app/static/images/background.webp'
-import Pools from './pools'
 import { Route } from 'react-router-dom'
+
+import { Row, Col } from 'antd'
+import Pools from './pools'
 import { useAppRouter } from 'app/hooks/useAppRouter'
 import PoolDetails from './poolDetails'
-import { AppDispatch, AppState } from 'app/model'
-import { increaseCounter } from 'app/model/main.controller'
-import configs from 'app/configs'
-import { createPDB } from 'shared/pdb'
-
-const {
-  manifest: { appId },
-} = configs
 
 const View = () => {
   const appRoute = useAppRouter()

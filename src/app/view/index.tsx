44f08447773c9991a6d0acc46831f6bd2b2994c3
dmcong <83624415+dmcong@users.noreply.github.com>
2022-05-26 18:00:46 +0700

    ..

--- src/app/view/index.tsx
+++ src/app/view/index.tsx
@@ -5,18 +5,26 @@ import { Row, Col } from 'antd'
 
 import BG from 'app/static/images/background.webp'
 import Pools from './pools'
+import { Route } from 'react-router-dom'
+import { useAppRouter } from 'app/hooks/useAppRouter'
+import PoolDetails from './poolDetails'
 
 const View = () => {
   const { setBackground } = useUI()
+  const { appRoute, params } = useAppRouter()
 
   useEffect(() => {
     setBackground({ light: BG, dark: BG })
   }, [setBackground])
 
+  console.log('params', params)
   return (
     <Row gutter={[24, 24]} align="middle" justify="center">
       <Col style={{ maxWidth: 1200 }} span={24}>
-        <Pools />
+        <Route exact path={`${appRoute}`} component={Pools} />
+        <Route exact path={`${appRoute}/pool/:poolId`} component={Pools}>
+          <PoolDetails />
+        </Route>
       </Col>
     </Row>
   )

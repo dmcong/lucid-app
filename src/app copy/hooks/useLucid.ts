9bba5352fa0cfbde79d19ef302c952aa6e6acd0c
dmcong <83624415+dmcong@users.noreply.github.com>
2022-05-26 18:31:19 +0700

    .

--- src/app copy/hooks/useLucid.ts
+++
@@ -1,8 +0,0 @@
-import { useWallet } from '@senhub/providers'
-import React, { useMemo } from 'react'
-
-export const useLucid = () => {
-  const { wallet } = useWallet()
-  const privder = useMemo(() => {}, [])
-  return null
-}
--- src/app copy/lib/constant.ts
+++
@@ -1,4 +0,0 @@
-import { IDL } from './lucid'
-
-export const DEFAULT_RPC_ENDPOINT = 'https://api.devnet.solana.com'
-export const DEFAULT_IDL = IDL

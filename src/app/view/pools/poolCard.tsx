c9e34a20ed714aa05e77d4d023edb902410d594b
thiennv <thiennguyen@descartes.network>
2022-05-28 15:45:29 +0700

    _

--- src/app/view/pools/poolCard.tsx
+++ src/app/view/pools/poolCard.tsx
@@ -11,7 +11,7 @@ import { AppState } from 'app/model'
 import { usePoolFees } from 'app/hooks/pool/usePoolFees'
 
 import './style.less'
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 
 type PoolCardProps = { rank: number; poolAddress: string }
 
@@ -26,87 +26,80 @@ const PoolCard = ({ rank, poolAddress }: PoolCardProps) => {
   }
 
   return (
-    <Card
-      bordered={false}
-      className={`pool-card top-${rank + 1}`}
-      onClick={onActive}
-    >
-      <Row>
-        <Col span={24}>
-          <Row gutter={[24, 24]} align="middle">
-            <Col>
-              <Typography.Title level={5}>{rank + 1}</Typography.Title>
-            </Col>
-            <Col>
-              <MintAvatar mintAddress={poolData.mint.toBase58()} size={48} />
-            </Col>
-            <Col span={4}>
-              <Space direction="vertical">
-                <Typography.Title level={5} ellipsis>
-                  <MintName mintAddress={poolData.mint.toBase58()} />
-                </Typography.Title>
-                <Typography.Title
-                  level={5}
-                  className="symbol"
-                  style={{ color: '#000' }}
-                >
-                  <MintSymbol mintAddress={poolData.mint.toBase58()} />
-                </Typography.Title>
-              </Space>
-            </Col>
-            <Col span={3}>
-              <CardContent
-                primary
-                label="APY"
-                value={numeric(Math.random()).format('0.00[00]%')}
-              />
-            </Col>
-            <Col span={4}>
-              <CardContent
-                label="Total Fee"
-                value={numeric(fee.totalFee).format('0,0.00[00]')}
-                mintAddress={poolData.baseMint.toBase58()}
-              />
-            </Col>
-            <Col span={4}>
-              <CardContent
-                label="Total Value Locked"
-                value={numeric(Math.random() * 100000).format('0,0.00[00]')}
-                mintAddress={poolData.baseMint.toBase58()}
-              />
-            </Col>
-            <Col span={4}>
-              <CardContent
-                label="Your Liquidity"
-                value={numeric(Math.random() * 100000).format('0,0.00[00]')}
-                mintAddress={poolData.baseMint.toBase58()}
-              />
-            </Col>
-            <Col>
-              <Button
-                type="text"
-                style={{ padding: 0, background: 'transparent' }}
-                onClick={() => {}}
+    <Fragment>
+      <Card
+        bordered={false}
+        className={`pool-card top-${rank + 1}`}
+        onClick={onActive}
+      >
+        <Row gutter={[24, 24]} align="middle">
+          <Col>
+            <Typography.Title level={5}>{rank + 1}</Typography.Title>
+          </Col>
+          <Col>
+            <MintAvatar mintAddress={poolData.mint.toBase58()} size={48} />
+          </Col>
+          <Col span={4}>
+            <Space direction="vertical">
+              <Typography.Title level={5} ellipsis>
+                <MintName mintAddress={poolData.mint.toBase58()} />
+              </Typography.Title>
+              <Typography.Title
+                level={5}
+                className="symbol"
+                style={{ color: '#000' }}
               >
-                <IonIcon
-                  name="arrow-forward-outline"
-                  style={{ fontSize: 32 }}
-                />
-              </Button>
-            </Col>
+                <MintSymbol mintAddress={poolData.mint.toBase58()} />
+              </Typography.Title>
+            </Space>
+          </Col>
+          <Col span={3}>
+            <CardContent
+              primary
+              label="APY"
+              value={numeric(Math.random()).format('0.00[00]%')}
+            />
+          </Col>
+          <Col span={4}>
+            <CardContent
+              label="Total Fee"
+              value={numeric(fee.totalFee).format('0,0.00[00]')}
+              mintAddress={poolData.baseMint.toBase58()}
+            />
+          </Col>
+          <Col span={4}>
+            <CardContent
+              label="Total Value Locked"
+              value={numeric(Math.random() * 100000).format('0,0.00[00]')}
+              mintAddress={poolData.baseMint.toBase58()}
+            />
+          </Col>
+          <Col span={4}>
+            <CardContent
+              label="Your Liquidity"
+              value={numeric(Math.random() * 100000).format('0,0.00[00]')}
+              mintAddress={poolData.baseMint.toBase58()}
+            />
+          </Col>
+          <Col>
+            <Button
+              type="text"
+              style={{ padding: 0, background: 'transparent' }}
+              onClick={() => {}}
+            >
+              <IonIcon name="arrow-forward-outline" style={{ fontSize: 32 }} />
+            </Button>
+          </Col>
+        </Row>
+      </Card>
+      <Collapse activeKey={activeKey} className="lucid-expand-card">
+        <Collapse.Panel header={null} key={poolAddress} showArrow={false}>
+          <Row gutter={[24, 24]}>
+            <Col>content</Col>
           </Row>
-        </Col>
-        <Col span={24}>
-          <Collapse activeKey={activeKey} className="lucid-expand-card">
-            <Collapse.Panel header={null} key={poolAddress} showArrow={false}>
-              <Row gutter={[24, 24]}>
-                <Col>content</Col>
-              </Row>
-            </Collapse.Panel>
-          </Collapse>
-        </Col>
-      </Row>
-    </Card>
+        </Collapse.Panel>
+      </Collapse>
+    </Fragment>
   )
 }
 
